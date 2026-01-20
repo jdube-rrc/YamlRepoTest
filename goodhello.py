550eb823529297fbe471e45d1fbb7d157fd6708f
Jared Dube <jdube3@academic.rrc.ca>
2026-01-20 12:32:52 -0600

    new files added

---
+++ goodhello.py
@@ -0,0 +1,17 @@
+def hello():
+    """
+    Prints a simple greeting.
+
+    """
+    print("Hello DevOps World.")
+
+def bye():
+    """
+    Prints a simple farewell.
+    
+    """
+    print("Goodbye DevOps World.")
+
+if __name__ == "__main__":
+    hello()
+    bye()
